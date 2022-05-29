f0009cc9f352aabfca9883bf4435e63fd5e85b90
dgfnight <degaloni@gmail.com>
2022-05-29 14:49:20 -0300

    Initial commit

---
+++ MinhaAppMvcCompleta/src/DevIO.Data/Repository/Repository.cs
@@ -0,0 +1,68 @@
+﻿using DevIO.Business.Models;
+using DevIO.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {
+        protected readonly MeuDbContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+
+        protected Repository(MeuDbContext db)
+        {
+            Db = db;
+            DbSet = db.Set<TEntity>();
+        }
+        public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+             
+
+        public virtual async Task<TEntity> ObterPorId(Guid Id)
+        {
+            return await DbSet.FindAsync(Id);
+        }
+
+        public virtual async Task<List<TEntity>> ObterTodos()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task Adicionar(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Atualizar(TEntity entity)
+        {
+            Db.Entry(entity).State = EntityState.Modified; // <---- LINHA ALTERADA
+            await SaveChanges();
+        }
+
+        public virtual async Task Remover(Guid id)
+        {
+            DbSet.Remove(new TEntity {Id = id });
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db?.Dispose();
+        }
+    }
+}
